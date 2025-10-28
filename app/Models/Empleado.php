0f43dd4abe663e4ae16d03a9853aa74438a961e2
jhenychirinos-png <jheny.chirinos@uab.edu.bo>
2025-10-27 22:50:07 -0400

    feat(api): ejecutar migraciones y seeders

--- app/Models/Empleado.php
+++ app/Models/Empleado.php
@@ -7,10 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Empleado extends Model
 {
     //
+    protected $table = 'empleados';
+
     protected $fillable = [
         'nombre',
         'apellido',
         'correo',
         'salario'
     ];
+    public function empleado()
+    {
+        return $this->hasMany(Empleado::class,'id');
+    }
 }
