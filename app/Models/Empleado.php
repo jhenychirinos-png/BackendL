bfed69d118dbb608d5d349bec1d551ae74c5dab6
jhenychirinos-png <jheny.chirinos@uab.edu.bo>
2025-10-27 21:41:09 -0400

    feat(api):crear modelo Empleado

---
+++ app/Models/Empleado.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Empleado extends Model
+{
+    //
+    protected $fillable = [
+        'nombre',
+        'apellido',
+        'correo',
+        'salario'
+    ];
+}
