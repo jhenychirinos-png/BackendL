ffb1372e3bab4acdfa62533fa3b9fc64e78ce7b1
jhenychirinos-png <jheny.chirinos@uab.edu.bo>
2025-10-30 16:13:15 -0400

    feat(api): crear controlador y rutas GET/POST empleados

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmpleadoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/api/empleados', [EmpleadoController::class, 'index']);
+// Route::post('/api/empleados', [EmpleadoController::class, 'store']);
+
+
+// php artisan make:migration create_empleados_table --create=empleados
+// php artisan db:seed
