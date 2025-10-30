5b4ba368913db783afba2a5c8187bc3814f7a33f
jhenychirinos-png <jheny.chirinos@uab.edu.bo>
2025-10-30 16:11:59 -0400

    feat(api): ejecutar routes y api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmpleadoController;
+
+Route::get('/test', function () {
+    return response()->json(['message' => 'API funciona']);
+}); 
 
 Route::get('/empleados', [EmpleadoController::class, 'index']);
 Route::post('/empleados', [EmpleadoController::class, 'store']);
